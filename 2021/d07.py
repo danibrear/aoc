4b21db7c2f9facffadf0da04b35e8f4ff13dc406
David Brear <david@fanscoutapp.com>
2021-12-07 00:21:18 -0500

    got day 7 parts 1 and 2

--- 2021/d07.py
+++ 2021/d07.py
@@ -1,19 +1,53 @@
-
 def part1(lines):
-    for line in lines:
-        pass
+    ps = list(map(int, lines[0].split(',')))
+    ps = sorted(ps)
+
+    m = len(ps)//2
+    medians = ps[m-10:m+10]
+
+    sm = float('inf')
+    for me in medians:
+        fuel = 0
+        for x in ps:
+            fuel += abs(x - me)
+        if fuel < sm:
+            sm = fuel
+    print(sm)
+
+
+def fact(n):
+    s = 0
+    for i in range(1, n+1):
+        s += i
+    return s
 
 
 def part2(lines):
-    for line in lines:
-        pass
+    smallest = float('inf')
+
+    ps = list(map(int, lines[0].split(',')))
+    ps = sorted(ps)
+    m = len(ps)//2
+    medians = ps[m-150:m+150]
+
+    for me in medians:
+        fuel = 0
+        for x in ps:
+            fuel += fact(abs(x - me))
+        if fuel < smallest:
+            smallest = fuel
+
+    print(smallest)
 
 
 with open('./d07.txt', 'r') as f:
 
+    from datetime import datetime
+
     lines = f.readlines()
     lines = list(map(lambda x: x.replace('\n', ''), lines))
 
+    print('Part 1: 335271:')
     part1(lines)
+    print('Part 2: 95851339:')
     part2(lines)
-
