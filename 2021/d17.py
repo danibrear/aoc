b7f7d033a95d2c928aad67542c3534571186e29d
David Brear <david@fanscoutapp.com>
2021-12-17 12:16:40 -0500

    cleaned up both parts to function in the same section and removed the method calls to speed it up

--- 2021/d17.py
+++ 2021/d17.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 testX = [20, 30]
 testY = [-10, -5]
 
@@ -5,106 +6,40 @@ realX = [207, 263]
 realY = [-115, -63]
 
 
-def step(vel, pos):
-    np = [pos[0] + vel[0], pos[1] + vel[1]]
-    nv0 = vel[0]
-    if nv0 > 0:
-        nv0 -= 1
-    if nv0 < 0:
-        nv0 += 1
-    nv1 = vel[1] - 1
+def parts1and2():
 
-    return [np, [nv0, nv1]]
-
-
-def intarget(pos, targetX, targetY):
-    inX = targetX[0] <= pos[0] <= targetX[1]
-    inY = targetY[0] <= pos[1] <= targetY[1]
-    return inX and inY
-
-
-def missed(pos, targetX, targetY):
-    intarg = intarget(pos, targetX, targetY)
-    if pos[1] < targetY[0] and not intarg:
-        return True
-
-
-def part1():
-    vel = [6, 9]
-    pos = [0, 0]
-
-    counter = 0
+    X = realX
+    Y = realY
     maxoverall = float('-inf')
 
-    X = realX
-    Y = realY
-
-    for x in range(0, X[0]):
-        for y in range(0, abs(Y[0])):
-            vel = [x, y]
-            pos = [0, 0]
-            mh = float('-inf')
-            hit = False
-            counter = 0
-            while counter < 300:
-
-                # print(start)
-                # print(step(start[1], start[0]))
-                (newpos, newvel) = step(vel, pos)
-                vel = newvel
-                pos = newpos
-                mh = max(mh, pos[1])
-                counter += 1
-                if intarget(pos, X, Y):
-                    hit = True
-                    # print("HIT:", counter, pos, mh)
-                    break
-                if missed(pos, X, Y):
-                    break
-            if hit:
-                maxoverall = max(maxoverall, mh)
-    print('Max height:', maxoverall)
-
-
-def part2():
-    vel = [6, 9]
-    pos = [0, 0]
-
-    counter = 0
-    vels = []
-
-    X = realX
-    Y = realY
+    vs = 0
 
     for x in range(0, X[1] + 1):
         for y in range(-abs(Y[0]), abs(Y[0])):
-            vel = [x, y]
-            iv = [x, y]
-            pos = [0, 0]
+            vx = x
+            vy = y
             mh = float('-inf')
+            px = 0
+            py = 0
             hit = False
-            counter = 0
-            while counter < 300:
-
-                # print(start)
-                # print(step(start[1], start[0]))
-                (newpos, newvel) = step(vel, pos)
-                vel = newvel
-                pos = newpos
-                mh = max(mh, pos[1])
-                counter += 1
-                if intarget(pos, X, Y):
+            while py >= Y[0] and px <= X[1]:
+                px += vx
+                py += vy
+                vx -= 1 if vx > 0 else 0
+                vy -= 1
+                mh = max(mh, py)
+                if X[0] <= px <= X[1] and Y[0] <= py <= Y[1]:
                     hit = True
                     # print("HIT:", counter, pos, mh)
                     break
-                if missed(pos, X, Y):
-                    break
             if hit:
-                vels.append(iv)
-    print('hits:', len(vels))
+                vs += 1
+                maxoverall = max(maxoverall, mh)
+
+    print('Part 1: [6555]', maxoverall)
+    print('Part 2: [4973]', vs)
 
 
-print('Part 1: [6555]')
-part1()
-print('Part 2: [4973]')
-part2()
+start = datetime.now()
+parts1and2()
+print(datetime.now() - start)
