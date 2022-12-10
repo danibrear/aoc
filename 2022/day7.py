8c21c874efcdd82bb2963f873b7c2e7a7319c309
David Brear <david@fanscoutapp.com>
2022-12-09 20:11:23 -0500

    day7

--- 2022/day7.py
+++ 2022/day7.py
@@ -71,8 +71,65 @@ def part1(lines):
 
 
 def part2(lines):
-    for line in lines:
-        pass
+    cwd = Dir(None, '', [], 0)
+    root = cwd
+    lineIdx = 0
+    while lineIdx < len(lines):
+        line = lines[lineIdx]
+        if line.startswith('$ cd'):
+            dir = line.split(' ')[-1].strip()
+            if (dir == '..'):
+                cwd = cwd.parent
+            else:
+                if dir not in cwd.dirs:
+                    cwd.dirs[dir] = Dir(cwd, dir, [], 0)
+                cwd = cwd.dirs[dir]
+            lineIdx += 1
+
+        elif line.startswith('$ ls'):
+            lineIdx = lineIdx + 1
+            while (lineIdx < len(lines) and
+                   not lines[lineIdx].startswith('$')):
+                line = lines[lineIdx]
+                if line.startswith('d'):
+                    dir = line.split(' ')[-1]
+                    if (dir not in cwd.dirs):
+                        cwd.dirs[dir] = Dir(cwd, dir, [], 0)
+                else:
+                    cwd.addfile(line)
+                lineIdx = lineIdx + 1
+
+    smallestToDelete = 10**10
+    total = 0
+
+    def gettotal(dir):
+        size = 0
+        nonlocal total
+        for f in dir.files:
+            total += int(f[1])
+        for d in dir.dirs.keys():
+            gettotal(dir.dirs[d])
+
+    gettotal(root)
+
+    USED = 70000000
+    NEEDED = 30000000
+    totalunused = USED - total
+
+    def printDirs(dir):
+        size = 0
+        nonlocal smallestToDelete
+        for f in dir.files:
+            size += int(f[1])
+        for d in dir.dirs.keys():
+            size += printDirs(dir.dirs[d])
+
+        if (totalunused + size >= NEEDED and size < smallestToDelete):
+            smallestToDelete = size
+        return size
+    printDirs(root)
+
+    print(smallestToDelete)
 
 
 with open('day7.txt', 'r') as f:
@@ -83,3 +140,4 @@ with open('day7.txt', 'r') as f:
 
     part1(lines)
     part2(lines)
+    print('GO FUCK YOURSELF ADVENT OF CODE')
